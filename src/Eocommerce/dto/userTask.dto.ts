f09ec04db0e7e2bc3944bda265ba527156c0ac06
Ashutosh Kumar <itsray650@gmail.com>
2025-06-18 18:26:48 +0530

    task2

--- src/Eocommerce/dto/userTask.dto.ts
+++ src/Eocommerce/dto/userTask.dto.ts
@@ -1,20 +1,37 @@
 import { Type } from "class-transformer";
-import { IsInt, IsString, ValidateNested } from "class-validator";
+import {
+    ArrayMaxSize,
+    ArrayUnique,
+    IsArray,
+    IsDate,
+    IsInt,
+    IsString,
+    ValidateNested
+} from "class-validator";
 
-export class startTime{
-    start:Date=new Date();
-}
 export class Tasks {
-    Tasks :Symbol;
-    start: Date=new Date();
-    end: startTime
+    @IsArray()
+    @ArrayUnique((task:Tasks)=>{task.TasksTitle},{ message: "Array must be unique" })
+    @ArrayMaxSize(5)
+    TasksTitle: string[];
+
+    @IsDate()
+    @Type(() => Date)
+    start: Date = new Date();
+
+    @IsDate()
+    @Type(() => Date)
+    end: Date = new Date(new Date().getTime());
 }
+
 export class User {
     @IsString()
     name: string;
+
     @IsInt()
-    age: 12;
-    @ValidateNested()
+    age: number;
+
+    @ValidateNested({ each: true })
     @Type(() => Tasks)
-    Task: [Tasks]
+    Task: Tasks[];
 }
